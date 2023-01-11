3ddb780d557c5f7e6caa618d730209c6935a567b
julio_filizzola <juliofilizzola@hotmail.com>
2023-01-11 16:20:15 -0300

    feat: create registe from serverless and func (update gitignore)

--- api/register/handler.js
+++ api/register/handler.js
@@ -1,16 +1,16 @@
-'use strict';
+'use strict'
 
-module.exports.hello = async (event) => {
+module.exports.register = async (event) => {
   return {
     statusCode: 200,
     body: JSON.stringify(
       {
-        message: 'Go Serverless v1.0! Your function executed successfully!',
+        message: process.env.DYNAMODB_USERS,
         input: event,
       },
       null,
       2
-    ),
+   ),
   };
 
   // Use this code if you don't use the http event with the LAMBDA-PROXY integration
