e0b81cc8835021c8ddb806a6a49d2f65bea7bf5b
julio_filizzola <juliofilizzola@hotmail.com>
2023-01-11 15:35:10 -0300

    feat: create table users from terraform

---
+++ terraform/infra/users/dynamodb-user.tf
@@ -0,0 +1,11 @@
+resource "aws_dynamodb_table" "users" {
+  name = "${var.environment}-users"
+  hash_key = "id"
+  attribute {
+    name = "id"
+    type = "S"
+  }
+  write_capacity = "${var.write_capacity}"
+  read_capacity = "${var.read_capacity}"
+
+}
