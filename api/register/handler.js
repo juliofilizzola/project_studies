79aa634a702628e162321c58b5d083693d88afa4
julio_filizzola <juliofilizzola@hotmail.com>
2023-01-14 16:16:59 -0300

    feat: add package uuid anda bycripty and create data in db

--- api/register/handler.js
+++ api/register/handler.js
@@ -1,18 +1,29 @@
 'use strict'
 
-module.exports.register = async (event) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: process.env.DYNAMODB_USERS,
-        input: event,
-      },
-      null,
-      2
-   ),
-  };
+const uuid = require('uuid');
+const bcrypt = require('bcryptjs');
+const AWS = require('aws-sdk');
+AWS.config.update({
+  region: process.env.AWS_REGION
+});
+const dbClient = new AWS.DynamoDB.DocumentClient();
 
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
-};
+module.exports.register = async (event) => {
+  const body = JSON.parse(event.body);
+  await dbClient.put({
+    TableName: process.env.DYNAMODB_USERS,
+    Item: {
+      id: uuid.v4(),
+      name: body.name,
+      email: body.email,
+      password: bcrypt.hashSync(body.password, 10)
+    }
+  }).promise();
+
+  return {
+    statusCode: 201,
+    body: JSON.stringify({
+      message: 'Usuário inserido com sucesso!',
+    }),
+  };
+}
