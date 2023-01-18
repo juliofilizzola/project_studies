527546f2f65ad8c7d69c9a7c9d430a23ad14742c
julio_filizzola <juliofilizzola@hotmail.com>
2023-01-18 10:28:51 -0300

    Feat: add global secundary Index

--- terraform/infra/users/dynamodb-user.tf
+++ terraform/infra/users/dynamodb-user.tf
@@ -5,6 +5,17 @@ resource "aws_dynamodb_table" "users" {
     name = "id"
     type = "S"
   }
+  
+  attribute {
+    name = "email"
+    type = "S"
+  }
+
+  global_secondary_index {
+    name = "${var.environment}-email.gsi"
+    projection_type = "ALL"
+    hash_key = "email"
+  }
   write_capacity = "${var.write_capacity}"
   read_capacity = "${var.read_capacity}"
 
