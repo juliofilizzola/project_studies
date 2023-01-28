e4aa4102c07b3bc7ff6b1b65735dfba1f577b949
julio_filizzola <juliofilizzola@hotmail.com>
2023-01-28 15:16:57 -0300

    REF: policy

--- terraform/infra/users/iam-policy-register.tf
+++ terraform/infra/users/iam-policy-register.tf
@@ -1,25 +1,9 @@
 resource "aws_iam_policy" "register_policy" {
   name = "${var.environment}-register-policy"
-
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = [
-          "dynamodb:PutItem",
-        ]
-        Effect   = "Allow"
-         "Resource": "${aws_dynamodb_table.users.arn}"
-      },
-      {
-        Effect = "Allow"
-        Action = [
-          "logs:GreateLogGroup",
-          "logs:CreateLogStream",
-          "logs:PutLogEvents"
-        ]
-        Resource: "*"
-      }
-    ]
+  
+  policy = templatefile("${path.module}/templates/dynamodb-policy.tpl", {
+    action = "dynamodb:PutItem"
+    resource = "${aws_dynamodb_table.users.arn}"
   })
+
 }
