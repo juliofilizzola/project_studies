853f474e228b744b091aa9a840c77f8a4e2f7790
julio_filizzola <juliofilizzola@hotmail.com>
2023-01-28 15:14:18 -0300

    feat: create template

--- terraform/infra/users/iam-policy-login.tf
+++ terraform/infra/users/iam-policy-login.tf
@@ -1,25 +1,8 @@
 resource "aws_iam_policy" "login_policy" {
   name = "${var.environment}-login-policy"
 
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = [
-          "dynamodb:Query",
-        ]
-        Effect   = "Allow"
-         "Resource": "${aws_dynamodb_table.users.arn}/index/${var.environment}-email.gsi"
-      },
-      {
-        Effect = "Allow"
-        Action = [
-          "logs:GreateLogGroup",
-          "logs:CreateLogStream",
-          "logs:PutLogEvents"
-        ]
-        Resource: "*"
-      }
-    ]
+  policy = templatefile("${path.module}/templates/dynamodb-policy.tpl", {
+    action = "dynamodb:Query"
+    resource = "${aws_dynamodb_table.users.arn}/index/${var.environment}-email.gsi"
   })
 }
